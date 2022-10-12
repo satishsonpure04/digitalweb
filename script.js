aba2c537829af16b36408392900ca20919a1add6
satishsonpure04 <114417546+satishsonpure04@users.noreply.github.com>
2022-10-12 07:26:20 -0700

    file upload

---
+++ script.js
@@ -0,0 +1,24 @@
+gsap.from("#imgx",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60
+})
+gsap.from("#imgy",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60
+})
+gsap.from("#imgz",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60
+})
+gsap.from("#main h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    
+})
